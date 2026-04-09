45f06e95b6fe42e47127df8206f179b936acc8be
FabianMp250822 <fabianmunozp250822@gmail.com>
2026-04-09 13:01:11 -0500

    correcion vercel

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // No complex webpack logic needed if isolation is correct
   serverExternalPackages: ["mysql2", "ioredis"],
-  devIndicators: {
-    appIsrStatus: false,
-  },
-  experimental: {
-    allowedDevOrigins: ["169.254.123.138", "localhost:3000"],
-  },
 };
 
 export default nextConfig;
